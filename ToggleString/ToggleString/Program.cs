886555addd7e854d95df397a47a17aac1569e83e
deepali <dbhavsar@tavisca.com>
2017-03-20 14:21:53 +0530

    toggle string

---
+++ ToggleString/ToggleString/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ToggleString
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string temp = string.Empty;
+            Console.WriteLine("Enter string");
+            string str = Console.ReadLine();
+            char[] charArray = str.ToCharArray();
+            for(int i=0;i<str.Length;i++)
+            {
+                if (char.IsLower(charArray[i]))
+                {
+                    temp = temp + charArray[i].ToString().ToUpper();
+                }
+                else
+                {
+                    temp = temp + charArray[i].ToString().ToLower();
+                }
+            }
+            Console.WriteLine(temp);
+            Console.ReadLine();
+
+        }
+    }
+}
